654bbda4f62d4605fd4c845190a010cb761a9c3a
psan3333 <59166366+psan3333@users.noreply.github.com>
2023-11-04 10:43:01 +0700

    Add files via upload

---
+++ module4/ex03/time_travel/launch/turtle_tf2_demo.launch.py
@@ -0,0 +1,48 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='turtlesim',
+            executable='turtlesim_node',
+            name='sim'
+        ),
+        Node(
+            package='time_travel',
+            executable='turtle_tf2_broadcaster',
+            name='broadcaster1',
+            parameters=[
+                {'turtlename': 'turtle1'}
+            ]
+        ),
+        DeclareLaunchArgument(
+            'target_frame', default_value='turtle1',
+            description='Target frame name.'
+        ),
+        DeclareLaunchArgument(
+            'delay', default_value='3',
+            description='Turtle pursuit delay in seconds.'
+        ),
+        Node(
+            package='time_travel',
+            executable='turtle_tf2_broadcaster',
+            name='broadcaster2',
+            parameters=[
+                {'turtlename': 'turtle2'}
+            ]
+        ),
+        Node(
+            package='time_travel',
+            executable='turtle_tf2_listener',
+            name='listener',
+            parameters=[
+                {'target_frame': LaunchConfiguration('target_frame')},
+                {'delay': LaunchConfiguration('delay')}
+            ]
+        ),
+    ])
